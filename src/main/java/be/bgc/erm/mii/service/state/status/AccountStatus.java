19c2a9c3431ebab2fce6c766e9c3a39a51eac6a7
michaelcouck-0-pxs <michael.couck.ext@proximus.com>
2017-09-28 01:01:05 +0200

    Adding the load test logic.

---
+++ src/main/java/be/bgc/erm/mii/service/state/status/AccountStatus.java
@@ -0,0 +1,12 @@
+package be.bgc.erm.mii.service.state.status;
+
+public enum AccountStatus {
+
+    VALID,
+    INVALID,
+    INVALID_CREDENTIALS,
+    TOKEN_EXPIRED,
+    ACCOUNT_LOCKED,
+    INVALID_SESSION
+
+}
--- src/main/java/com/pxs/reaper/Loader.java
+++ src/main/java/com/pxs/reaper/Loader.java
@@ -1,41 +1,70 @@
 package com.pxs.reaper;
 
+import be.bgc.erm.mii.service.state.status.AccountStatus;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 import ikube.toolkit.THREAD;
 
-import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 
 public class Loader {
 
     public static void main(final String[] args) {
         THREAD.initialize();
 
-        for (int i = 0; i < 10; i++) {
-            THREAD.submit("", () -> {
-                Client client = Client.create();
-                WebResource webResource = client.resource("uri");
+        /*final String authUser = "id851622";
+        final String authPassword = "Caherl2ne";
+        Authenticator.setDefault(
+                new Authenticator() {
+                    @Override
+                    public PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(
+                                authUser, authPassword.toCharArray());
+                    }
+                }
+        );
+        System.setProperty("http.proxyUser", authUser);
+        System.setProperty("http.proxyPassword", authPassword);*/
 
-                WebResource.Builder builder = webResource.getRequestBuilder();
-                builder.cookie(new Cookie("", ""));
+        THREAD.submit("fls-login", () -> {
+        });
 
-                MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
-                queryParams.add("json", ""); //set parameters for request
+        // String appKey = "Bearer kmjhsdqfohqdsfmkjh";
+        // .header("Authorization", appKey)
 
-                String appKey = "Bearer kmjhsdqfohqdsfmkjh"; // appKey is unique number
+        MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
+        queryParams.add("username", "bartvv");
+        queryParams.add("password", "instanet01");
 
-                ClientResponse response = null;
-                response = webResource.queryParams(queryParams)
-                        .header("Content-Type", "application/json;charset=UTF-8")
-                        .header("Authorization", appKey)
-                        .get(ClientResponse.class);
+        // ?username=bartvv&password=instanet01
 
-                String jsonStr = response.getEntity(String.class);
-            });
-        }
+        // https://sphinx.skynet.be/rest/billing-account-overview-aggregator
+        // https://sphinx.skynet.be/mii/identify/explicit/fls
+        Client client = Client.create();
+        WebResource webResource = client
+                .resource("https://sphinx.skynet.be/mii/identify/explicit/fls?username=bartvv&password=instanet01"); // ?username=bartvv&password=instanet01
+        /*String response = webResource
+                .queryParams(queryParams)
+                .entity("{}")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Content-Type", MediaType.APPLICATION_JSON) // application/json;charset=UTF-8
+                .post(String.class);*/
+        // String jsonStr = response.getEntity(String.class);
+
+        Object secondResponse =webResource
+                // .type("application/x-www-form-urlencoded")
+                // .queryParams(queryParams)
+                .accept(MediaType.APPLICATION_JSON)
+                // .type(MediaType.APPLICATION_FORM_URLENCODED)
+                .post(String.class, queryParams);
+        System.out.println(secondResponse);
+        // System.out.println(secondResponse.getEntity(String.class));
+        // System.out.println(response);
     }
 
 }
