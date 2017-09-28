f38d2de7c71a4a915d62e901947277ffc5616f93
michaelcouck <michael.couck@gmail.com>
2017-09-28 02:05:04 +0200

    Re-structuring the loader.

--- src/main/java/com/pxs/reaper/Loader.java
+++ src/main/java/com/pxs/reaper/Loader.java
@@ -1,6 +1,5 @@
 package com.pxs.reaper;
 
-import be.bgc.erm.mii.service.state.status.AccountStatus;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
@@ -16,8 +15,31 @@ public class Loader {
 
     public static void main(final String[] args) {
         THREAD.initialize();
+        THREAD.submit("fls-login", () -> {
+        });
 
-        /*final String authUser = "id851622";
+        new LoginToProxy().login();
+
+        MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
+        queryParams.add("username", "bartvv");
+        queryParams.add("password", "instanet01");
+
+        Client client = Client.create();
+        WebResource webResource = client.resource("https://sphinx.skynet.be/mii/identify/explicit/fls");
+
+        Object secondResponse = webResource
+                .queryParams(queryParams)
+                .accept(MediaType.APPLICATION_JSON)
+                .type(MediaType.APPLICATION_FORM_URLENCODED)
+                .post(ClientResponse.class);
+        System.out.println(secondResponse);
+    }
+
+}
+
+class LoginToProxy {
+    void login() {
+        final String authUser = "id851622";
         final String authPassword = "Caherl2ne";
         Authenticator.setDefault(
                 new Authenticator() {
@@ -29,42 +51,11 @@ public class Loader {
                 }
         );
         System.setProperty("http.proxyUser", authUser);
-        System.setProperty("http.proxyPassword", authPassword);*/
-
-        THREAD.submit("fls-login", () -> {
-        });
-
-        // String appKey = "Bearer kmjhsdqfohqdsfmkjh";
-        // .header("Authorization", appKey)
-
-        MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
-        queryParams.add("username", "bartvv");
-        queryParams.add("password", "instanet01");
-
-        // ?username=bartvv&password=instanet01
+        System.setProperty("http.proxyPassword", authPassword);
 
         // https://sphinx.skynet.be/rest/billing-account-overview-aggregator
         // https://sphinx.skynet.be/mii/identify/explicit/fls
-        Client client = Client.create();
-        WebResource webResource = client
-                .resource("https://sphinx.skynet.be/mii/identify/explicit/fls?username=bartvv&password=instanet01"); // ?username=bartvv&password=instanet01
-        /*String response = webResource
-                .queryParams(queryParams)
-                .entity("{}")
-                .accept(MediaType.APPLICATION_JSON)
-                .header("Content-Type", MediaType.APPLICATION_JSON) // application/json;charset=UTF-8
-                .post(String.class);*/
-        // String jsonStr = response.getEntity(String.class);
-
-        Object secondResponse =webResource
-                // .type("application/x-www-form-urlencoded")
-                // .queryParams(queryParams)
-                .accept(MediaType.APPLICATION_JSON)
-                // .type(MediaType.APPLICATION_FORM_URLENCODED)
-                .post(String.class, queryParams);
-        System.out.println(secondResponse);
-        // System.out.println(secondResponse.getEntity(String.class));
-        // System.out.println(response);
+        // String appKey = "Bearer kmjhsdqfohqdsfmkjh";
+        // .header("Authorization", appKey)
     }
-
 }
