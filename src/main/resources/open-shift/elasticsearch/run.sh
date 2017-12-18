99a3490f7de978ab51c8ad4383c9a8c52fa9c190
michaelcouck <michael.couck@gmail.com>
2017-12-18 14:57:19 +0100

    Setting the system properties for the reaper in OpenShift.

--- src/main/resources/open-shift/elasticsearch/run.sh
+++ src/main/resources/open-shift/elasticsearch/run.sh
@@ -1,26 +1,25 @@
 #! /bin/bash -e
 
-echo 'Starting elastic search'
-# update-rc.d elasticsearch defaults 95 10
-/etc/init.d/elasticsearch start
+# System variables to set on command line or in OpenShift for the reaper
+# JMX_URI=service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi
+# WEB_SOCKET_URI=ws://ikube.be:8090/reaper-websocket
+# REAPER_ZIP=https://ikube.be/artifactory/libs-release-local/com/pxs/reaper-agent/1.0-SNAPSHOT/reaper-agent-1.0-SNAPSHOT-linux.zip
 
-ps -ef 
-
-# echo 'Enabling elastic search'
-# /bin/systemctl daemon-reload
-# /bin/systemctl enable elasticsearch.service
-# echo 'Starting elastic search again'
-# /bin/systemctl start elasticsearch.service
-
-# ls -lh
-# ls -lh /root
-# ls -lh /root/lib
-
+# Sanity check
 java -version
 echo $JAVA_HOME
-java -Dlocalhost-jmx-uri=service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi -Dreaper-web-socket-uri=ws://ikube.be:8090/reaper-websocket -jar /root/reaper-agent-1.0-SNAPSHOT.jar
-echo 'Started the reaper'
 
-# echo 'Starting the reaper'
-# cd /root
-# find . -maxdepth 1 -name "*.jar" | xargs -n1 java -jar
+# We must be root
+cd /root
+
+# Install wget first
+apt-get -y install wget unzip
+
+# Get the agent zip and unpack it
+wget --no-check-certificate --no-proxy $REAPER_ZIP
+unzip reaper-agent-1.0-SNAPSHOT-linux.zip
+chmod 777 -R *
+cd reaper
+
+ls -l
+java -Dlocalhost-jmx-uri=$JMX_URI -Dreaper-web-socket-uri=$WEB_SOCKET_URI -jar reaper-agent-1.0-SNAPSHOT.jar
